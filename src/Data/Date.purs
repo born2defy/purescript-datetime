654ca4d8bf2d58224b9a30adf934d52ceca14f16
born2defy <born2defy@gmail.com>
2016-03-30 10:10:45 -0400

    Added a few extra functions

--- src/Data/Date.purs
+++ src/Data/Date.purs
@@ -4,12 +4,15 @@ module Data.Date
   , endOfMonth, beginningOfMonth
   , mkDate
   , calcEscrows, calcTaxEscrows
+  , daysAfterNow, daysBeforeNow
+  , daysAfter, daysBefore
+  , prettyDate, purePrettyDate
   ) where
 
 import Prelude
 import Control.Monad.Eff (Eff())
 import Data.Date.JulianGregorian as JT
-import Data.Time (Year(), Month(), DayOfWeek(), Month(November), advanceMonths)
+import Data.Time (Year(), DayOfWeek(), Month(November), advanceMonths)
 import Data.Date.NativeDate as Native
 import Data.Function (on)
 import Data.Enum (fromEnum)
@@ -17,22 +20,46 @@ import Data.Enum (fromEnum)
 -- | By default, a `Date`is represented as a `JulianTime` value
 type Date = JT.JulianTime
 
+-- | Pretty Print the date
+prettyDate :: Date -> String
+prettyDate = JT.prettyJulianTime
+
+-- | Convenience function for pretty printing dates based on Now
+purePrettyDate :: forall eff. Date -> Eff eff String
+purePrettyDate = pure <<< prettyDate
+
 -- | Gets a `Date` value for the current date/time according to the current
 -- | machine’s local time.
-now :: forall eff. Eff (now :: Native.Now | eff) Date
+now :: forall eff. Eff (now::Native.Now | eff) Date
 now = Native.now >>= pure <<< JT.dateToJulianTime
 
+-- | Get the date x number of days from now
+daysAfterNow :: forall eff. Int -> Eff (now::Native.Now | eff) Date
+daysAfterNow n = now >>= pure <<< JT.addDaysJT n
+
+-- | Get the date x number of days prior to now
+daysBeforeNow :: forall eff. Int -> Eff (now::Native.Now | eff) Date
+daysBeforeNow n = now >>= pure <<< JT.addDaysJT (-n)
+
 -- | Gets the current year from the local machine
-getCurrentYear :: forall eff. Eff (now :: Native.Now | eff) Year
+getCurrentYear :: forall eff. Eff (now::Native.Now | eff) Year
 getCurrentYear = Native.getCurrentYear
 
 -- | Gets the current month from the local machine
-getCurrentMonth :: forall eff. Eff (now :: Native.Now | eff) Month
+getCurrentMonth :: forall eff. Eff (now::Native.Now | eff) Month
 getCurrentMonth = Native.getCurrentMonth
 
 -- | Gets the current day from the local machine
-getCurrentDay :: forall eff. Eff (now :: Native.Now | eff) DayOfWeek
+getCurrentDay :: forall eff. Eff (now::Native.Now | eff) DayOfWeek
 getCurrentDay = Native.getCurrentDay
+
+-- | Get the date x number of days from a given date.  Meant to be used infix
+daysAfter :: Int -> Date -> Date
+daysAfter = JT.addDaysJT
+
+-- | Get the date x number of days prior to a given date.  Meant to be used infix
+daysBefore :: Int -> Date -> Date
+daysBefore n = JT.addDaysJT (-n)
 
 -- | Takes a `date` and returns a new `date` representing the last day of that month
 endOfMonth :: Date -> Date
